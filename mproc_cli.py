3513bcce6e4731b071a078ce3495acd94520cc63
Simone <simofoti@gmail.com>
2023-06-19 15:52:02 +0100

    Add command line interface for existing functions

---
+++ mproc_cli.py
@@ -0,0 +1,31 @@
+import fire
+
+import registration
+
+
+def fire_no_out_print(component=None, command=None, name=None, serialize=None):
+    try:
+        fire.Fire(component, command, name, serialize)
+    except ModuleNotFoundError as error_msg:
+        if "open3d" in str(error_msg):
+            # Fire tries to Print the trimesh result. Apparently this requires
+            # additional libraries (i.e. open3d). Since the error is raised
+            # only using the CLI and it does not affect the correct execution of
+            # the code, the known error is detected and ignored.
+            pass
+        else:
+            print(f"something went wrong wile fire was running the code"
+                  f"the following error has been reported: {str(error_msg)}")
+
+
+if __name__ == "__main__":
+    fire_no_out_print({
+        "procrustes_landmark_registration":
+            registration.ProcrustesLandmarkRegisterer,
+        "inertia_axes_and_icp_registration":
+            registration.InertiaAxesAndIcpRegisterer,
+        "procrustes_landmark_and_icp_registration":
+            registration.ProcrustesLandmarkAndIcpRegisterer,
+        "procrustes_landmark_and_nicp_registration":
+            registration.ProcrustesLandmarkAndNicpRegisterer,
+    })
